180641487e919cce33977f937d27d45c3eb50e9c
DefinitelyNotAnAssassin <dharklike@gmail.com>
2024-05-15 16:59:13 +0800

    3RD

--- app/src/main/java/com/lms/CourseList.java
+++ app/src/main/java/com/lms/CourseList.java
@@ -1,24 +1,94 @@
 package com.lms;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class CourseList extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class CourseList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    Menu menu;
+    TextView textView;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_course_list);
+        setContentView(R.layout.activity_courses_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //hooks
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+
+        //toolbar
+        setSupportActionBar(toolbar);
+
+
+        //nav drawer
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.homeMenu) {
+            // Handle homeMenu
+        } else if (id == R.id.joinCourseMenu) {
+            Intent intent = new Intent(CourseList.this, enroll_course.class);
+            startActivity(intent);
+        } else if (id == R.id.finishedActsMenu) {
+            Intent intent = new Intent(CourseList.this, FinishedActivities.class);
+            startActivity(intent);
+        } else if (id == R.id.missingActsMenu) {
+            Intent intent = new Intent(CourseList.this, MissingActivities.class);
+            startActivity(intent);
+        }
+
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+
     }
 }
--- app/src/main/java/com/lms/MainActivity.java
+++ app/src/main/java/com/lms/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_courses_main);
+        setContentView(R.layout.activity_course_list);
 
     }
 }
