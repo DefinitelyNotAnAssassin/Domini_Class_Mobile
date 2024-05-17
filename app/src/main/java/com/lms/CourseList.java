3cb6b068caaad2d72a9d4c40b79022429c964b6a
shewwy <blythesherry14@gmail.com>
2024-05-17 19:22:04 +0800

    PROTOTYPE

--- app/src/main/java/com/lms/CourseList.java
+++ app/src/main/java/com/lms/CourseList.java
@@ -37,10 +37,13 @@ import java.util.Map;
 public class CourseList extends AppCompatActivity {
 
 
-    DrawerLayout drawerLayout;
+
 
     ListView lvEnrolledCourseList;
     String currentUser;
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     ArrayList<Map<String, String>> courses = new ArrayList<>();
     @Override
@@ -52,6 +55,26 @@ public class CourseList extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+
+            // Handle the actions for the different menu items here
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
         });
 
 
@@ -71,31 +94,31 @@ public class CourseList extends AppCompatActivity {
                 Request.Method.POST,
                 "http://dominiclass.pythonanywhere.com/course/getCourse",
                 response -> {
-                 try {
-    JSONObject jsonResponse = new JSONObject(response);
-    JSONArray courseArray = jsonResponse.getJSONArray("course");
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        JSONArray courseArray = jsonResponse.getJSONArray("course");
 
-                     System.out.println(courseArray);
+                        System.out.println(courseArray);
 
-    for (int x = 0; x < courseArray.length(); x++) {
-        JSONObject course = courseArray.getJSONObject(x);
-        Map<String, String> courseMap = new HashMap<>();
-        courseMap.put("courseName", course.getString("course_name"));
-        courseMap.put("courseDescription", course.getString("course_description"));
-        courseMap.put("couresCode", course.getString("course_code"));
-        courseMap.put("id", course.getString("id"));
-        courses.add(courseMap);
-    }
-
-
-                     CustomCoursesItemAdapter adapter = new CustomCoursesItemAdapter(this, courses, currentUser);
-                     lvEnrolledCourseList.setAdapter(adapter);
+                        for (int x = 0; x < courseArray.length(); x++) {
+                            JSONObject course = courseArray.getJSONObject(x);
+                            Map<String, String> courseMap = new HashMap<>();
+                            courseMap.put("courseName", course.getString("course_name"));
+                            courseMap.put("courseDescription", course.getString("course_description"));
+                            courseMap.put("couresCode", course.getString("course_code"));
+                            courseMap.put("id", course.getString("id"));
+                            courses.add(courseMap);
+                        }
 
 
+                        CustomCoursesItemAdapter adapter = new CustomCoursesItemAdapter(this, courses, currentUser);
+                        lvEnrolledCourseList.setAdapter(adapter);
 
-} catch (JSONException e) {
-    e.printStackTrace();
-}
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 },
                 error -> {
                     System.out.println(error);
